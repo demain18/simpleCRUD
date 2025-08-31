a8d37824e7bb62b89df6a78752119854b159b84a
Brian <happysugar180@gmail.com>
2025-08-31 17:09:35 +0900

    add: 화면 구성 수정

--- app/(tabs)/write.tsx
+++ app/(tabs)/write.tsx
@@ -2,16 +2,29 @@ import StyledButton from "@/components/atoms/StyledButton";
 import Gnb from "@/components/organisms/gnb/Gnb";
 import WritePost from "@/components/organisms/write/WritePost";
 import { colors } from "@/hooks/colorScheme";
-import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 export interface Props {}
 
 export default function Write({ ...rest }: Props) {
   return (
-    <View style={styles.mainWrap}>
-      <Gnb />
-      <WritePost />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+    >
+      <View style={styles.mainWrap}>
+        <Gnb />
+        <WritePost />
+      </View>
+    </KeyboardAvoidingView>
   );
 }
 
